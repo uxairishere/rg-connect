648957d5aa1553f667e6d5f773384ad6cf370f71
Uxair Abbas <uzaaaaaair@gmail.com>
2024-05-23 10:52:44 +0500

    refactor(validation): Added trim in form validation

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,16 +1,16 @@
 import * as z from 'zod';
 
 export const formSchema = z.object({
-    profileId: z.string().min(1, {
+    profileId: z.string().trim().min(1, {
         message: "Profile ID is required",
     }),
-    loginCode: z.string().min(1, {
+    loginCode: z.string().trim().min(1, {
         message: "Login code is required",
     }),
-    username: z.string().min(1, {
+    username: z.string().trim().min(1, {
         message: "Username is required",
     }),
-    password: z.string().min(1, {
+    password: z.string().trim().min(1, {
         message: "Password is required",
     }),
 })
